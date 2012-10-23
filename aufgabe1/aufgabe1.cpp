dd9c2f9501a4f4274b2bf8573bfc16824c435be0
Franz Torghele <f.torghele@gmail.com>
2012-10-23 16:22:11 +0200

    aufgabe1

---
+++ aufgabe1/aufgabe1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "merge_lists.cpp"
+
+void fill_list(intlist &list) {
+    for(unsigned int i = 0; i <= 1000; i = i + ((std::rand()%100)+1)) {
+        list.push_front(i);
+    }
+}
+
+void print_list(intlist &list) {
+    intlist::const_iterator iterator;
+    for (iterator = list.begin(); iterator != list.end(); ++iterator) {
+        std::cout << *iterator << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    std::srand((unsigned)time(0));
+
+    intlist a, b, c;
+    intlist lists[3];
+
+    fill_list(a);
+    fill_list(b);
+    fill_list(c);
+
+    lists[0] = a;
+    lists[1] = b;
+    lists[2] = c;
+
+    std::cout << "Lists which should be merged together:" << std::endl;
+    for(unsigned int i = 0; i < 3; i++) {
+        print_list(lists[i]);
+    }
+
+    std::cout << "Merged List:" << std::endl;
+    intlist merged_lists = merge_lists(lists, 3);
+    print_list(merged_lists);
+
+    return 0;
+}
